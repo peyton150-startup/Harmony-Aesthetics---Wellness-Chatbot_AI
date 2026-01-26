c075cf236a7e8e6aa4e33a209d18823f779753d7
peyton150-startup <peyton150150@gmail.com>
2026-01-26 11:22:05 -0500

    Update server.js
    server port

--- backend/server.js
+++ backend/server.js
@@ -75,7 +75,8 @@ app.get("/health", (req, res) => {
 });
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
